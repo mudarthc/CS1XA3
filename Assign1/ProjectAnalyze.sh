27e91b8a41be580131c70901cbda6a7573b0071b
Christina Mudarth <mudarthc@mills.cas.mcmaster.ca>
2018-02-20 19:08:35 -0500

    last5

--- Assign1/ProjectAnalyze.sh
+++ Assign1/ProjectAnalyze.sh
@@ -3,16 +3,18 @@
 clear
 
 echo "Welcome! what is your name?"
+FS=":" read -p "enter your name " \VAR1 
+            
+echo "Hi $VAR1, you are currently here: $(pwd)" 
 
-echo "Hi,you are currently here: $(pwd)" 
-git status
+#git status
 #part1 
 
 #part2
-git diff >>  changes.log
+#git diff >>  changes.log
 
 #part3
-grep "#TODO" . -R  >> todo.log
+#grep "#TODO" . -R  >> todo.log
 
 #part4 
-ghc -fno-code *.hs &>> error.log 
+#ghc -fno-code *.hs &>> error.log 
